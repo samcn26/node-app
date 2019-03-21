f25db610860e41cad4a37b0332d4f78f37af5f66
sam <470289074@qq.com>
2019-03-21 13:20:44 +0900

    body parser: parse form data

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express")
-var exphbs  = require('express-handlebars');
-
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
 const app = express();
+// body-parser
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
 
 // handlebars middleware
 app.engine('handlebars', exphbs({
@@ -10,24 +15,46 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars');
 
 // 配置路由
-app.get("/",(req,res) =>{
+app.get("/", (req, res) => {
     const title = "Hello Everybody"
     //render 到具体文件
-    res.render("index",{
-        title:title
-    });   
-}) 
-
-app.get("/about",(req,res) =>{
-    res.render("about");   
+    res.render("index", {
+        title: title
+    });
 })
 
-// app.get("/test",(req,res) =>{
-//     res.render("TEST");   
-// })
+app.get("/about", (req, res) => {
+    res.render("about");
+})
+
+app.get("/ideas/add", (req, res) => {
+    res.render("ideas/add");
+})
+
+app.post("/ideas", urlencodedParser, (req, res) => {
+    // console.log(req.body);
+    let errors = [];
+
+    if (!req.body.title) {
+        errors.push({ text: "please add title" })
+    } 
+    if(!req.body.details){
+        errors.push({ text: "please add details" })
+    }
+
+    if (errors.length>0){
+        res.render("ideas/add",{
+            errors:errors,
+            title:req.body.title,
+            details:req.body.details
+        });
+    }else{
+        res.send("ok");
+    }
+})
 
 const port = 5000;
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server started on ${port}`)
 })
